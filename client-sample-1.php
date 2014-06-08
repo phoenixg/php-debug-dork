a9020f22ea74bd8bbffaa355c367ecd8a65b1939
phx <306761352@qq.com>
2014-06-08 15:10:28 +0800

    a

--- client-sample-1.php
+++ client-sample-1.php
@@ -1,12 +1,21 @@
 <?php 
-
+$vpsIp = '128.199.233.252';
 
 $dataFromDb = array(
 	'a' => array('aaa', 'aaaaa', 'aaaaaaaaaa'),
 	'b' => array('bbb', 'bbbbb', 'bbbbbbbbbb'),
 );
 
-$dataFromDbEncoded = serialize($dataFromDb);
+$url = 'http://' . $vpsIp . '/php-debug-dork/debug.php';
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($dataFromDb),
+    ),
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
 
-echo $dataFromDbEncoded;
 
+var_dump($result);

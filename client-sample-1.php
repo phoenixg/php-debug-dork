e7ca3fcb69103dc53f1bd07269e3a0930576a5af
phx on do1 <306761352@qq.com>
2014-06-13 07:46:25 +0000

    a

--- client-sample-1.php
+++ client-sample-1.php
@@ -1,16 +1,33 @@
 <?php 
+
 $vpsIp = '128.199.233.252';
 $url = 'http://' . $vpsIp . '/php-debug-dork/debug.php';
 
 // 模拟真实的需调试打印的数据
+class test{ 
+   private $_a;
+   public $b;
+
+   protected function _test1(){
+     return array('11','22');
+   } 
+
+   public function test2(){
+     return 'test222';
+   }
+}
+
+$test = new test();
 $dataFromDb = array(
 	'a' => array('aaa', 'aaaaa', 'aaaaaaaaaa'),
 	'b' => array('bbb', 'bbbbb', 'bbbbbbbbbb'),
+        'c' => $test,
+        'd' => $test->test2(),
+        'e' => true,
 );
 
 // $dataFromDbEncoded = urlencode(serialize( $dataFromDb ));
 $dataFromDbEncoded = $dataFromDb;
-
 
 $ch = curl_init();
 
@@ -22,7 +39,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $server_output = curl_exec($ch);
 
-curl_close ($ch);
+curl_close($ch);
 
 // further processing ....
 // if ($server_output == "OK") { ... } else { ... }
--- debug.php
+++ debug.php
@@ -1,4 +1,5 @@
 <?php 
+// 本脚本负责将POST过来的数据用Kint输出后，存储到report.php
 ob_start();
 
 error_reporting(E_ALL);
@@ -7,8 +8,8 @@ ini_set('display_errors', 'on');
 require './kint09/Kint.class.php';
 
 // $postDebugData = unserialize(urldecode( file_get_contents('php://input') ));
-$postDebugData = file_get_contents('php://input');
+// $postDebugData = file_get_contents('php://input');
 
-Kint::dump( $postDebugData );
+Kint::dump( $_POST );
 $dump = ob_get_clean();
 file_put_contents('report.php', $dump);

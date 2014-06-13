54b12b57c69afc8cbe677e8a699b679c38dff5e5
phx on do1 <306761352@qq.com>
2014-06-13 06:14:27 +0000

    a

--- debug.php
+++ debug.php
@@ -1,3 +1,17 @@
 <?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+
+require './kint09/Kint.class.php';
+
+
+
+
+
+
 $O = unserialize(urldecode($_POST['a']));
-var_dump($O);
+Kint::dump( $O );
+
+
+
